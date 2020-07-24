9ca940c282915bbf9aefcdf42ec90a5116fa56f3
Ebubekir Sezer <ebubekirssezer@gmail.com>
2020-07-24 15:55:44 +0300

    People view added to the project

---
+++ httpsample/lib/view/people_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PeopleView extends StatefulWidget {
+  @override
+  _PeopleViewState createState() => _PeopleViewState();
+}
+
+class _PeopleViewState extends State<PeopleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
