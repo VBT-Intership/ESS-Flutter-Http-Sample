801c7643d92a54d661f16617b960a5b3a7fb5902
Ebubekir Sezer <ebubekirssezer@gmail.com>
2020-07-24 19:13:08 +0300

    Unnecessary things removed

--- httpsample/lib/viewmodels/people_view_model.dart
+++ httpsample/lib/viewmodels/people_view_model.dart
@@ -2,15 +2,12 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:httpsample/models/people_model.dart';
-import 'package:httpsample/models/person_model.dart';
 import 'package:httpsample/view/people_view.dart';
 import 'package:http/http.dart' as http;
 
 abstract class PeopleViewModel extends State<PeopleView> {
   List users = [];
   final String BASE_URL = "https://reqres.in/";
-  //final String BASE_URL = "https://hwasampleapi.firebaseio.com//";
 
   @override
   void initState() {
@@ -20,20 +17,13 @@ abstract class PeopleViewModel extends State<PeopleView> {
   }
 
   Future<void> getPeople() async {
-    //final response = await http.get(BASE_URL + "http.json");
     final response = await http.get(BASE_URL + "api/users?page=2");
     print(response.statusCode);
     switch (response.statusCode) {
       case HttpStatus.ok:
         final jsonBody = jsonDecode(response.body);
-        List _users = [];
-        users = jsonBody["data"];
         if (jsonBody["data"] is List) {
-          _users = jsonBody["data"];
-          users = _users;
-          for (var user in users) {
-            print(user["first_name"]);
-          }
+          users = jsonBody["data"];
         }
         break;
       default:
